447c15731938e24aba070abb062eb2336b047e45
Jeonghwan <jh327373@gmail.com>
2025-07-12 00:25:41 +0900

    fmain

---
+++ server.js
@@ -0,0 +1,41 @@
+const { createServer } = require('https')
+const { parse } = require('url')
+const next = require('next')
+const fs = require('fs')
+
+const dev = process.env.NODE_ENV !== 'production'
+const hostname = '0.0.0.0'  // 모든 IP에서 접속 허용
+const port = 3000
+
+// when using middleware `hostname` and `port` must be provided below
+const app = next({ dev, hostname, port })
+const handle = app.getRequestHandler()
+
+const httpsOptions = {
+  key: fs.readFileSync('./13.209.50.98+2-key.pem'),
+  cert: fs.readFileSync('./13.209.50.98+2.pem'),
+}
+
+app.prepare().then(() => {
+  createServer(httpsOptions, async (req, res) => {
+    try {
+      // Be sure to pass `true` as the second argument to `url.parse`.
+      // This tells it to parse the query portion of the URL.
+      const parsedUrl = parse(req.url, true)
+      const { pathname, query } = parsedUrl
+
+      await handle(req, res, parsedUrl)
+    } catch (err) {
+      console.error('Error occurred handling', req.url, err)
+      res.statusCode = 500
+      res.end('internal server error')
+    }
+  })
+    .once('error', (err) => {
+      console.error(err)
+      process.exit(1)
+    })
+    .listen(port, () => {
+      console.log(`> Ready on https://${hostname}:${port}`)
+    })
+})
