6316e000230b21556beb3e68306c1199f5dc033f
이윤아 <qwer120542@gmail.com>
2025-07-20 21:56:47 +0900

    PronunciationTimingGuide 게이지바 아래 글씨 삭제

--- src/components/dubbing/PronunciationTimingGuide.tsx
+++ src/components/dubbing/PronunciationTimingGuide.tsx
@@ -181,7 +181,6 @@ export default function PronunciationTimingGuide({
                             }}
                           />
                         </div>
-                        <div className="text-xs text-gray-400 mt-1">{Math.round(animatedScore * 100)}%</div>
                       </div>
                     );
                   })}
